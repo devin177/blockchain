3ca377dd5877a087fb596a95eab74b8ec4c94368
devin177 <ly.devin.177@gmail.com>
2021-07-01 15:18:41 -0700

    some changes to transactions

--- transaction.h
+++ transaction.h
@@ -5,12 +5,25 @@
 
 using namespace std;
 
+// individual inputs
 class TxIn {
+ public:
+  // Identify the output we are trying to spend
+  string TxId;
+  uint32_t index;
+
+  // signature script that will satisfy pubkey script
 
 };
 
+// individual outputs
+// This is the record that makes up people's wallets
+// Transactions are stored in the UTXO set?
 class TxOut {
-
+ public:
+  // implied index from location in vector
+  int64_t Amount;
+  // Conditional pubkey script
 };
 
 class Transaction {
@@ -24,10 +37,10 @@ class Transaction {
   // Version/convention of transaction
   int32_t version;
   // Array of inputs
-  vector<TxIn> in;
+  vector<TxIn> vin;
 
   // Array of outputs
-  vector<TxOut> out;
+  vector<TxOut> vout;
   /* no partial spending?
   * Add up your incoming tx - outgoing tx for balance
   * For partials, send some?
