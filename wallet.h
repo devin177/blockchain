289780ef701db53c534584e0bf2546fc6108372f
devin177 <ly.devin.177@gmail.com>
2021-07-29 21:53:51 -0700

    Generate a wallet address with new function: generateaddress

--- wallet.h
+++ wallet.h
@@ -13,6 +13,8 @@
 #include <openssl/sha.h>
 #include <openssl/ripemd.h>
 
+#include "base58.h"
+
 // calling an unsigned char a byte
 typedef unsigned char byte;
 
@@ -25,41 +27,18 @@ class Wallet {
   Wallet() = default;
 };
 
-/* See https://en.wikipedia.org/wiki/Positional_notation#Base_conversion */
-char* base58(byte *s, int s_size, char *out, int out_size) {
-  static const char *base_chars = "123456789"
-    "ABCDEFGHJKLMNPQRSTUVWXYZ"
-    "abcdefghijkmnopqrstuvwxyz";
-
-  byte s_cp[s_size];
-  memcpy(s_cp, s, s_size);
-
-  int c, i, n;
-
-  out[n = out_size] = 0;
-  while (n--) {
-    for (c = i = 0; i < s_size; i++) {
-      c = c * 256 + s_cp[i];
-      s_cp[i] = c / 58;
-      c %= 58;
-    }
-    out[n] = base_chars[c];
-  }
-
-  return out;
-}
-
-void GeneratePrivKey() {
+void GenerateAddress() {
   ctx = secp256k1_context_create(
 	SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
 
-  unsigned char seckey[32];
+  // Private keys are 256 bits = 32 bytes
+  unsigned char privkey[32];
 
   /* Load private key (seckey) from random bytes */
   FILE *frand = fopen("/dev/urandom", "r");
 
   /* Read 32 bytes from frand */
-  fread(seckey, 32, 1, frand);
+  fread(privkey, 32, 1, frand);
 
   /* Close the file */
   fclose(frand);
@@ -67,24 +46,28 @@ void GeneratePrivKey() {
   /* Loop through and print each byte of the private key, */
   printf("Private Key: ");
   for(int i=0; i<32; i++) {
-    printf("%02X", seckey[i]);
+    printf("%02X", privkey[i]);
   }
   printf("\n");
 
-  if (!secp256k1_ec_seckey_verify(ctx, seckey)) {
+  if (!secp256k1_ec_seckey_verify(ctx, privkey)) {
 	  printf("Invalid secret key\n");
-    // error for invalid
+    return;
   }
 
-  // Use elliptic curve to generate pubkey
+  // Generate a pubkey
   secp256k1_pubkey pubkey;
-  int temp = secp256k1_ec_pubkey_create(ctx, &pubkey, seckey);
+  int temp = secp256k1_ec_pubkey_create(ctx, &pubkey, privkey);
+
+  // error check
   if (temp == 0) {
-    ;
+    cout << "Invalid Public Key. Try Again";
+    return;
   }
 
+  // UNCOMPRESSED bitcoin public keys are 65 bytes, the first of which is 0x04
   size_t pk_len = 65;
-  unsigned char pk_bytes[34];
+  unsigned char pk_bytes[65];
 
   /* Serialize Public Key */
   secp256k1_ec_pubkey_serialize(
@@ -95,14 +78,23 @@ void GeneratePrivKey() {
     SECP256K1_EC_UNCOMPRESSED
     );
 
+  // Print public key ~ 65 bytes, but in hex-2byte
+  cout << "Public Key: ";
+    for(int i=0; i<65; i++) {
+      printf("%02X", pk_bytes[i]);
+    }
+  cout << endl;
+
   // Turn public key(in bytes) to bitcoin address
   // char pubaddress[34];
-  byte s[65];
+
+  byte dupe_pubkey[65];
+  // RipeMD hash will be 20, 1 for version byte, 4 for checksum
   byte rmd[5 + RIPEMD160_DIGEST_LENGTH];
 
   // Copy public key into temp var s
   for (int j = 0; j < 65; j++) {
-	  s[j] = pk_bytes[j];
+	  dupe_pubkey[j] = pk_bytes[j];
   }
 
   // 1. Take SHA256 hash of pubkey
@@ -110,43 +102,27 @@ void GeneratePrivKey() {
   // 3. Add a version byte in front (lets say 0x00)
   /* Set 0x00 byte for main net */
   rmd[0] = 0;
-  RIPEMD160(SHA256(s, 65, 0), SHA256_DIGEST_LENGTH, rmd + 1);
+  RIPEMD160(SHA256(dupe_pubkey, 65, 0), SHA256_DIGEST_LENGTH, rmd + 1);
 
-  // 4. Create checksum by SHA256 hashing twice
-  SHA256(SHA256(rmd, 21, 0), SHA256_DIGEST_LENGTH, 0);
+  cout << "Version + RIPEMD-160 Hash of Public Key: ";
+    for(int i=0; i<21; i++) {
+      printf("%02X", rmd[i]);
+    }
+  cout << endl;
 
-  // 4.5. We only care about the last 4 bytes of the checksum, add them to the end
+  // 4. Create checksum by SHA256 hashing the VERSION_BYTE+RIPEMD_HASH
+  // 4.5. Append the first 4 bytes of doublehash to the end of original RIPEMD
   memcpy(rmd + 21, SHA256(SHA256(rmd, 21, 0), SHA256_DIGEST_LENGTH, 0), 4);
 
-  // 5. Store our Address in a char[34]
-  // convert from bytes to base 58 string
-  char address[34];
-  base58(rmd, 25, address, 34);
+  cout << "Address in Hex: ";
+    for(int i=0; i<25; i++) {
+      printf("%02X", rmd[i]);
+    }
+  cout << endl;
 
-  // leading 1's in base 58 don't count, so remove them
-    /* Count the number of extra 1s at the beginning of the address */
-  int k;
-  for (k = 1; address[k] == '1'; k++);
-
-  /* Count the number of extra leading 0x00 bytes */
-  int n;
-  for (n = 1; rmd[n] == 0x00; n++);
-
-  /* Remove k-n leading 1's from the address */
-  memmove(address, address + (k-n), 34-(k-n));
-  address[34-(k-n)] = '\0';
-
-  printf("Address: %s\n", address);
-
-  // checksum
-  byte tempadd[21];
-  for (int i = 0; i < 21; ++i) {
-    tempadd[i] = address[i];
-  }
-
-  SHA256(SHA256(tempadd, 21, 0), SHA256_DIGEST_LENGTH, 0);
-
-  printf("Checksum: %s\n", tempadd);
+  // 5. Convert from bytes to base 58 string
+  string address = EncodeBase58(rmd, rmd+25);
+  cout << "Base58 Encoded Address: " << address << endl;
 }
 
 #endif
