403d7aa536259fb80345a485afbcabfc0bdf5e9f
devin177 <ly.devin.177@gmail.com>
2021-08-05 16:54:00 -0700

    Working on coinbase tx

--- transaction.h
+++ transaction.h
@@ -21,22 +21,38 @@ class TxIn {
 // Transactions are stored in the UTXO set?
 class TxOut {
  public:
+  TxOut(int64_t amount, string address): Amount(amount),
+    Address(address){
+      ;
+    };
+
   // implied index from location in vector
   int64_t Amount;
+
   // Conditional pubkey script
   // For now, I will just put a address that I will check is the same
+  string Address;
+  void spend() {Spent = true;}
+ private:
+  bool Spent = false;
 };
 
 class Transaction {
  public:
-  // Constructor
-  Transaction();
+  // Normal transaction
+  Transaction(vector<TxIn> inputs, vector<TxOut> outputs);
+
+  // Coinbase transaction to one miner
+  Transaction(string address) {
+    TxOut CBTx(50, address);
+    vout.push_back(CBTx);
+  }
 
   // Hash of the TX -> will be used to find merkle root
   string TXHash;
 
   // Version/convention of transaction
-  int32_t version;
+  // int32_t version;
   // Array of inputs
   vector<TxIn> vin;
 
