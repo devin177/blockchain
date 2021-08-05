bb0b6e6e7815cf2adf59c569365e5acd1bf293be
devin177 <ly.devin.177@gmail.com>
2021-08-05 16:54:48 -0700

    changed name, no longer node.cpp

---
+++ walletclient.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "blockchain.h"
+#include <stdio.h>
+#include <secp256k1.h>
+#include "wallet.h"
+#include <sstream>
+
+using namespace std;
+
+int main () {
+  Blockchain bchain = Blockchain();
+  cout << bchain.GetSize() << endl;
+  
+  string line_buffer;
+  string cmd_buff;
+  cout << ">";
+  while (getline(cin, line_buffer)) {
+    stringstream line_stream(line_buffer);
+    line_stream >> cmd_buff;
+    if (!cmd_buff.compare("addblock")) {
+      bchain.AddBlock(bchain.GetLastHash(), bchain.GetLastBlock().GetTransactions());
+      cout << bchain.GetSize() << endl;
+    } else if (!cmd_buff.compare("generatewallet")) {
+      GenerateWalletFile();
+    } else if (!cmd_buff.compare("hashseed")){
+      line_stream >> cmd_buff;
+      HashPrivSeed(cmd_buff);
+    } else {
+      cout << "Closing program" << endl;
+      break;
+    }
+    cout << ">";
+  }
+
+  return 0;
+}
+
+// Add transactions to the blocks
+// First one to add: Coinbase transaction ~ the distribution of reward coins
