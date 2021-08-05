3110fa6c6eab0294038e26c6f40a756a5cc8fcd1
devin177 <ly.devin.177@gmail.com>
2021-08-05 16:54:29 -0700

    working on verifying addresses

--- wallet.h
+++ wallet.h
@@ -8,12 +8,13 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-// stuff from tutorial
+#include <sstream>
+// Imports for turning privkey to address
 #include <secp256k1.h>
 #include <openssl/sha.h>
 #include <openssl/ripemd.h>
-
 #include "base58.h"
+#include "hex.h"
 
 // calling an unsigned char a byte
 typedef unsigned char byte;
@@ -112,5 +113,68 @@ void GenerateWalletFile() {
 // Parse through the blockchain and update your wallet
 void UpdateWallet() {
   ;
+}
+
+// Given a seeded private key, find its public address
+void HashPrivSeed(string privkey){
+  byte seed[32];
+  string string_val = hex_to_string(privkey);
+  for (int i = 0; i < 32; ++i) {
+    seed[i] = string_val[i];
+  }
+  ctx = secp256k1_context_create(
+	  SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+
+    if (!secp256k1_ec_seckey_verify(ctx, seed)) {
+	  printf("Invalid secret key\n");
+    return;
+  }
+
+  // Generate a pubkey
+  secp256k1_pubkey pubkey;
+  int temp = secp256k1_ec_pubkey_create(ctx, &pubkey, seed);
+
+    // error check
+  if (temp == 0) {
+    cout << "Invalid Public Key. Try Again";
+    return;
+  }
+
+  size_t pk_len = 65;
+  unsigned char pk_bytes[65];
+
+  /* Serialize Public Key */
+  secp256k1_ec_pubkey_serialize(
+    ctx,
+    pk_bytes,
+    &pk_len,
+    &pubkey,
+    SECP256K1_EC_UNCOMPRESSED
+    );
+
+  // Turn public key(in bytes) to bitcoin address
+  byte dupe_pubkey[65];
+  // RipeMD hash will be 20, 1 for version byte, 4 for checksum
+  byte rmd[5 + RIPEMD160_DIGEST_LENGTH];
+
+  // Copy public key into temp var s
+  for (int j = 0; j < 65; j++) {
+	  dupe_pubkey[j] = pk_bytes[j];
+  }
+
+  // 1. Take SHA256 hash of pubkey
+  // 2. Take RIPEMD160 hash of ^
+  // 3. Add a version byte in front (lets say 0x00)
+  /* Set 0x00 byte for main net */
+  rmd[0] = 0;
+  RIPEMD160(SHA256(dupe_pubkey, 65, 0), SHA256_DIGEST_LENGTH, rmd + 1);
+
+  // 4. Create checksum by SHA256 hashing the VERSION_BYTE+RIPEMD_HASH
+  // 4.5. Append the first 4 bytes of doublehash to the end of original RIPEMD
+  memcpy(rmd + 21, SHA256(SHA256(rmd, 21, 0), SHA256_DIGEST_LENGTH, 0), 4);
+
+  // 5. Convert from bytes to base 58 string
+  string address = EncodeBase58(rmd, rmd+25);
+  cout << address << endl;
 }
 #endif
